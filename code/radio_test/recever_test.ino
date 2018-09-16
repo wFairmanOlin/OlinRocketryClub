ce524e8b7c7233fc1187503bf5fd2045d06d20f2
Anusha Datar <anushadatar@gmail.com>
2018-09-16 15:07:09 -0400

    added test.ino files

---
+++ code/radio_test/recever_test.ino
@@ -0,0 +1,49 @@
+// Olin Rocketry
+// 434 MHz Receiver test code. Useful mainly for testing antenna functionality.
+// RF receiver used - https://www.sparkfun.com/products/10532 - matches piece on 
+// actual flight PCB, but breadboarded to arduino pin 2 and to experimental
+// antenna for testing.
+// This code is heavily adapted from their examples.
+
+// TODO Phase out using this library. Should only be a few lines of code, but will
+//      require testing with known working antenna.
+
+// Requires library from http://www.airspayce.com/mikem/arduino/VirtualWire/VirtualWire-1.27.zip
+// or contact Anusha/Will/Jane for zip file and instruction.#include <VirtualWire.h>
+#include <VirtualWire.h>
+int radio_rx_pin = 2;
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Initializing receiver module.");
+
+  vw_set_ptt_inverted(true); // Unclear but this has to be set for it to work.
+  vw_setup(2000);     // bps
+  vw_set_rx_pin(radio_rx_pin);      
+  vw_rx_start();         
+
+  // LEDs are good.
+  pinMode (13, OUTPUT);
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  // Non-blocking operation, just wait for receipt.
+  if (vw_get_message(buf, &buflen)) 
+  {
+    // Flash if data received.
+    digitalWrite(13, true); 
+    Serial.print("Received message: ");
+    for (int i = 0; i < buflen; i++){
+      Serial.print(buf[i]);
+      }
+    }
+    Serial.println("");
+    digitalWrite(13, false);
+}
+
+
+
---
+++ code/radio_test/transmitter_test.ino
@@ -0,0 +1,37 @@
+// Olin Rocketry
+// 434 MHz Transmitter test code. Useful mainly for testing antenna functionality.
+// RF transmitter used: https://www.sparkfun.com/products/10534 - matches piece on 
+// actual flight PCB, but breadboarded to arduino pin 2 for testing.
+// This code is heavily adapted from their examples.
+
+// TODO Phase out using this library. Should only be a few lines of code, but will
+//      require testing with known working antenna.
+// Requires library from http://www.airspayce.com/mikem/arduino/VirtualWire/VirtualWire-1.27.zip
+// or contact Anusha/Will/Jane for zip file and instruction.
+#include <VirtualWire.h>
+
+// TX pin for radio.
+int radio_tx_pin = 2;
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Initializing TX");
+
+  vw_set_ptt_inverted(true); // Unsure why this is needed, but it is.
+  vw_setup(2000);     // bps
+  vw_set_tx_pin(radio_tx_pin);        
+
+  // LEDs are good.
+  pinMode (13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, true);  // Status LEDs are important.
+  char *msg = "1"; // The message to send over the air.
+  vw_send((uint8_t *)msg, strlen(msg));
+  Serial.println("Sending message");
+  vw_wait_tx(); 
+  digitalWrite(13, false); 
+}
